b4e32de2c3e9b76201cb6141a02d81bf2abd0958
chepecarlos <chepecarlos@alswblog.org>
2013-11-22 18:06:22 -0600

    Reparando los nombres

--- AddCliente.php
+++ AddCliente.php
@@ -5,7 +5,9 @@ require("SQL.php");
 	
 	<?php
 	 if($_POST){
-		  	$queEmp = "INSERT INTO `veterinaria`.`prop_propietarios` (`prop_id`, `prop_nombre1`, `prop_nombre2`, `prop_apellido1`, `prop_apellido2`, `prop_fnacimiento`, `prop_direccion`, `prop_telcasa`, `prop_telcel`, `prop_teltrabajo`, `prop_email`, `prop_dui`, `prop_nit`, `prop_regfiscal`, `gen_genero_gen_id`) VALUES ('".$_POST["ID"]."', '".$_POST["Nombre1"]."', '".$_POST["Nombre2"]."', '".$_POST["Apellido1"]."', '".$_POST["Apellido2"]."', NULL, '".$_POST["Direcion"]."', '".$_POST["TelefonoCasa"]."', NULL, NULL, NULL, '".$_POST["DUI"]."', '".$_POST["NIT"]."', NULL, '1');";
+		  	$queEmp = "INSERT INTO `veterinaria`.`prop_propietarios` (`prop_id`, `prop_nombre1`, `prop_nombre2`, `prop_apellido1`, `prop_apellido2`, `prop_fnacimiento`, `prop_direccion`, `prop_telcasa`, `prop_telcel`, `prop_teltrabajo`, `prop_email`, `prop_dui`, `prop_nit`, `prop_regfiscal`, `gen_genero_gen_id`) 
+		  	VALUES (null, '".$_POST["Nombre1"]."', '".$_POST["Nombre2"]."', '".$_POST["Apellido1"]."', '".$_POST["Apellido2"]."','".$_POST["Fecha"]."', '".$_POST["Direcion"]."', '".$_POST["TelefonoCasa"]."', NULL, NULL, NULL, '".$_POST["DUI"]."', '".$_POST["NIT"]."', NULL, '1');";
+			//echo $queEmp."<br>";
 			$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error()); 
 			echo "<p> Creacion de Cliente Satisfecha </p>";
 		 }
@@ -14,11 +16,11 @@ require("SQL.php");
 				<center><p>
 	<form name="formulario" method="POST" onSubmit='return Connprobacion()' action="AddCliente.php">
 	<div align="center">
-	<p>ID: <input type="text" name="ID"></p>
-	<p>Nombre 1: <input type="text" name="Nombre1"></p>
-	<p>Nombre 2: <input type="text" name="Nombre2"></p>
-	<p>Apellido 1: <input type="text" name="Apellido1"></p>
-	<p>Apellido 2: <input type="text" name="Apellido2"></p>
+	<p>Primer Nombre: <input type="text" name="Nombre1"></p>
+	<p>Segundo Nombre : <input type="text" name="Nombre2"></p>
+	<p>Primer Apellido: <input type="text" name="Apellido1"></p>
+	<p>Segundo Apellido: <input type="text" name="Apellido2"></p>
+	<p>Fecha de nacimiento (formato año-mes-dia)<input type="text" name="Fecha"></p>
 	<p>Direcion: <input type="text" name="Direcion"></p>
 	<p>Telefono Casa: <input type="text" name="TelefonoCasa"></p>
 	<p>DUI: <input type="text" name="DUI"></p>
