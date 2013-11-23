7e7003a2e64129e231981afcf6f2b065ed0f62fe
chepecarlos <chepecarlos@alswblog.org>
2013-11-22 19:15:32 -0600

    Casi terminaod formulario

--- AddMascota.php
+++ AddMascota.php
@@ -16,15 +16,68 @@ require("SQL.php");
 				<center><p>
 	<form name="formulario" method="POST" onSubmit='return Connprobacion()' action="AddCliente.php">
 	
-	<p>ID: <input type="text" name="ID"></p>
-	<p>Nombre 1: <input type="text" name="Nombre1"></p>
-	<p>Nombre 2: <input type="text" name="Nombre2"></p>
-	<p>Apellido 1: <input type="text" name="Apellido1"></p>
-	<p>Apellido 2: <input type="text" name="Apellido2"></p>
-	<p>Direcion: <input type="text" name="Direcion"></p>
-	<p>Telefono Casa: <input type="text" name="TelefonoCasa"></p>
-	<p>DUI: <input type="text" name="DUI"></p>
-	<p>NIT: <input type="text" name="NIT"></p>
+	<p>Primer nombre: <input type="text" name="Nombre1"></p>
+	<p>Segundo nombre: <input type="text" name="Nombre2"></p>
+	<p>Primer Apelliso: <input type="text" name="Apellido1"></p>
+	<p>Segundo Apellido: <input type="text" name="Apellido2"></p>
+	<p>Nacimiento(Año-Mes-Dia): <input type="text" name="Nacimiento"></p>
+	<p>Descripcion: <input type="text" name="Descripcion"></p>
+	<p>Especie: 
+	<select name="Especie">
+		<?php
+		$queEmp = "SELECT * FROM `mas_mascotas`";
+		$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
+		while ($row = mysql_fetch_row($resEmp)){
+			echo "<option value=".$row[0]." selected>".$row[1]."</option>";
+			}
+		?>
+	</select>
+	</p>
+	<p>Familia:
+	<select name="Familia">
+		<?php
+		$queEmp = "SELECT * FROM `fam_familia`";
+		$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
+		while ($row = mysql_fetch_row($resEmp)){
+			echo "<option value=".$row[0]." selected>".$row[1]."</option>";
+			}
+		?>
+	</select>
+	</p>
+	<p>Genero: 
+	<select name="Genero">
+		<?php
+		$queEmp = "SELECT * FROM `gen_genero`";
+		$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
+		while ($row = mysql_fetch_row($resEmp)){
+			echo "<option value=".$row[0]." selected>".$row[1]."</option>";
+			}
+		?>
+	</select>
+	
+	<p>Estado: 
+	<select name="Estado">
+		<?php
+		$queEmp = "SELECT * FROM `esm_estado_mascotas`";
+		$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
+		while ($row = mysql_fetch_row($resEmp)){
+			echo "<option value=".$row[0]." selected>".$row[1]."</option>";
+			}
+		?>
+	</select>
+	</p>
+	<p>EStado salida?:
+	<select name="EstadoSalida">
+		<?php
+		$queEmp = "SELECT * FROM `tps_tipo_salida`";
+		$resEmp = mysql_query($queEmp, $conexion) or die(mysql_error());
+		while ($row = mysql_fetch_row($resEmp)){
+			echo "<option value=".$row[0]." selected>".$row[1]."</option>";
+			}
+		?>
+	</select>
+	</p>
+	<p>Fecha de ingreso(Año-Mes-Dia): <input type="text" name="FechaIngeso"></p>
 	
 	<p><input type="submit" value="Ingresar"></p>
 	</form>
